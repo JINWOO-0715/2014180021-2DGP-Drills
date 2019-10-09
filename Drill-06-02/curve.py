1b322d53f8d075ba63f413233f8946680998b67e
JINWOO-0715 <pjw1480@naver.com>
2019-10-09 18:58:18 +0900

    화면 캐릭터띄우기
    2014180021 박진우 Drill-06-02
    커밋 횟수 : 1회

--- Drill-06-02/curve.py
+++ Drill-06-02/curve.py
@@ -3,17 +3,7 @@ from pico2d import *
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
 
-def handle_events():
-   global running
-   global x, y
-   events = get_events()
-   for event in events:
-        if event.type == SDL_QUIT:
-            running = False
-        elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, KPU_HEIGHT - 1 - event.y
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
+
 open_canvas(KPU_WIDTH,  KPU_HEIGHT)
 
 
@@ -33,7 +23,7 @@ while running:
     update_canvas()
     frame = (frame + 1) % 8
 
-    handle_events()
+
 
 close_canvas()
 
