c228a973057a12e85bbb4b4f69a4f7a786c841be
JINWOO-0715 <pjw1480@naver.com>
2019-10-09 17:29:20 +0900

    풀지못함
    미풀이

--- Drill-05/work.py
+++ Drill-05/work.py
@@ -3,19 +3,18 @@ import random
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
 
-def handle_events():
+
+def mouse_handle_events():
     global running
-    global x, y
+    global mx,my
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
            running = False
-        elif event.type == SDL_MOUSEBUTTONDOWN:
-              x, y = event.x, KPU_HEIGHT - 1 - event.y
-
+        elif event.type == SDL_MOUSEMOTION:
+              mx, my = event.x, KPU_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
               running = False
-
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
@@ -24,24 +23,31 @@ mouse = load_image('hand_arrow.png')
 
 running = True
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+mx, my = KPU_WIDTH // 2, KPU_HEIGHT // 2
+x2, y2 = KPU_WIDTH // 2, KPU_HEIGHT // 2
 
 show_cursor()
-size =40
-p1 = [(random.randint(50,500)) for i in range(size)]
-p2 = [(random.randint(0,500)) for i in range(size)]
-while running:
+size =999
+p1 = [(random.randint(-500, 500) ) for i in range(size)]
+p2 = [(random.randint(-500, 500)) for i in range(size)]
+
+while True:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    p1 = mx,my
-
+    mouse.clip_draw(0,0,50,50,mx,my)
+    update_canvas()
+    mouse_handle_events()
+    p1[0] = (mx,my)
+    p2[0] = (mx,my)
     for i in range(0, 100 + 1, 2):
         t = i / 100
-        x = (1 - t) * p1[0] + t * p2[0]
-        y = (1 - t) * p1[1] + t * p2[1]
-        character.clip_draw( 100, 100 * 1, 100, 100, x, y)
-    update_canvas()
+        x = (1-t)*p1[0]+t*p2[0]
+        y = (1-t)*p1[1]+t*p2[1]
+        character.clip_draw(100, 100 * 1, 100, 100, x, y)
+        update_canvas()
 
-
-    handle_events()
 
 close_canvas()
+
+
+
