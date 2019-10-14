e1d7b70f916c3c0e2d8a3ff06cb12d19f6504ef4
JINWOO-0715 <pjw1480@naver.com>
2019-10-14 18:36:21 +0900

    클래스 구현
    2014180021 박진우 Drill-07
    커밋 횟수: 1회

---
+++ Drill-07/boy_grass_object.py
@@ -0,0 +1,62 @@
+from pico2d import *
+import random
+# Game object class here
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png');
+
+    def draw(self):
+        self.image.draw(400,30)
+class Boy :
+    def __init__(self):
+        self.x,self.y = random.randint(100,700),90
+        self.frame = random.randint(0,7)
+        self.image = load_image('run_animation.png')
+    def update(self):
+        self.frame = (self.frame+1)%8
+        self.x +=5
+    def draw(self):
+        self.image.clip_draw(self.frame*100 ,0,100,100,self.x,self.y)
+
+class Ball:
+    def __init__(self):
+        self.x,self.y = random.randint(50,750),700
+        self.image_small_ball = load_image('ball21x21.png')
+        self.move_speed =  random.randint(3,12)
+        self.image_big_ball = load_image('ball41x41.png')
+        self.shape = random.randint(1,2)
+    
+
+        pass
+
+# initialization code
+open_canvas()
+boy = Boy()
+grass = Grass()
+team = [Boy() for i in range(11)]
+running = True
+
+# game main loop code
+while running:
+    handle_events() # 플레이어
+    for boy in team:
+        boy.update()
+    clear_canvas()
+    for boy in team:
+        boy.draw()
+    grass.draw()
+    update_canvas()
+
+    delay(0.05)
+
+# finalization code
+close_canvas()
