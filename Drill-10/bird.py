703246f9322de8dafd4570301abccde8d3c5961f
unknown <pjw1480@naver.com>
2019-11-12 21:00:19 +0900

    Drill-10 Sucess
    Drill-10 Sucess
    2014180021 박진우
    커밋횟수 7회
    https://www.youtube.com/watch?v=AAy8XQuvN0Y&feature=youtu.be

--- Drill-10/bird.py
+++ Drill-10/bird.py
@@ -2,8 +2,8 @@ from pico2d import *
 import game_world
 import game_framework
 
-PIXEL_PER_METER = (10.0 / 0.03)  # 10 pixel 30 cm
-RUN_SPEED_KMPH = 2  # Km / Hour
+PIXEL_PER_METER = (30.0 / 0.1)  # 30 pixel 10 cm
+RUN_SPEED_KMPH = 4.0  # Km / Hour
 RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
@@ -12,8 +12,8 @@ RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 # Boy Action Speed
 # fill expressions correctly
 TIME_PER_ACTION = 0.5
-ACTION_PER_TIME = 1.0
-FRAMES_PER_ACTION = 14
+ACTION_PER_TIME = 1.0 /TIME_PER_ACTION
+FRAMES_PER_ACTION = 5
 
 
 class Bird:
@@ -25,18 +25,22 @@ class Bird:
         self.x, self.y, self.velocity = x, y, velocity
         self.dir = 1
         self.frame = 0
+        self.frame_x = 0
+        self.frame_y = 0
 
     def draw(self):
         if self.dir == 1:
-            self.image.clip_composite_draw((int(self.frame) % 5) * 182, (int((15 - self.frame) / 5)) * 167, 182, 167, 0,
+            self.image.clip_composite_draw(int(self.frame_x) * 182, int(self.frame_y) * 167, 182, 167, 0,
                                            '', self.x, self.y, 182, 167)
         else:
-            pass
-
+            self.image.clip_composite_draw(int(self.frame_x) * 182, int(self.frame_y) * 167, 182, 167, 0,
+                                           'h', self.x, self.y, 182, 167)
 
     def update(self):
         self.x += self.velocity * game_framework.frame_time
         self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 14
+        self.frame_x = self.frame % 5
+        self.frame_y = (15-self.frame) / 5
         if self.dir == 1:
             self.velocity = RUN_SPEED_PPS
         if self.dir == 2:
