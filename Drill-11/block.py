ea1728e5ccaf0b180793c31fd298a62b188c39de
unknown <pjw1480@naver.com>
2019-11-14 15:57:21 +0900

    블록함수 추가및 생성완료
    블록 왔다갔다 구현 완료

---
+++ Drill-11/block.py
@@ -0,0 +1,21 @@
+
+import game_framework
+from pico2d import *
+
+class Block:
+    def __init__(self):
+        self.x, self.y = 800, 170
+        self.image = load_image('brick180x40.png')
+        self.velocity = 1
+
+    def get_bb(self):
+        return self.x - 90, self.y - 20, self.x + 90, self.y + 20
+
+    def update(self):
+        self.x += self.velocity
+        if self.x < 90 or 1600 - 90 < self.x:
+            self.velocity = -self.velocity
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+        draw_rectangle(*self.get_bb())
